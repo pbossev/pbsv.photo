be1bf39674286cf75770bf540e65b96a0f5ff861
pbossev <peterbossev@gmail.com>
2025-10-22 00:41:36 -0400

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/content/**/*.jpg");
   eleventyConfig.addPassthroughCopy("src/content/**/*.webp");
-  // eleventyConfig.addPassthroughCopy({ "src/assets/favicon": "/" });
+  eleventyConfig.addPassthroughCopy({ "src/assets/favicon": "/" });
 
   return {
     dir: {
